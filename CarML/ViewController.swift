79a501e52b62aecfd128e6f22f27ad03a8ccd208
ALbert Duran <albertrdg@gmail.com>
2018-03-28 17:21:04 -0300

    se formateo el valor del slider de kms

--- CarML/ViewController.swift
+++ CarML/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var modelSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var extrasSwitch: UISwitch!
+    @IBOutlet weak var kmsLabel: UILabel!
+    @IBOutlet weak var kmsSlider: UISlider!
+    @IBOutlet weak var statusSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    let car = Cars()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.stackView.setCustomSpacing(25, after: self.modelSegmentedControl)
+        self.stackView.setCustomSpacing(25, after: self.extrasSwitch)
+        self.stackView.setCustomSpacing(25, after: self.kmsSlider)
+        self.stackView.setCustomSpacing(50, after: statusSegmentedControl)
+        
+        self.calculatedValue()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func calculatedValue() {
+        
+        //formatear el valor del slider
+        let formater = NumberFormatter()
+        formater.numberStyle = .decimal
+        formater.maximumFractionDigits = 0
+        let formatedKms = formater.string(for: self.kmsSlider.value) ?? "0"
+        self.kmsLabel.text = "Kilometraje: \(formatedKms) kms"
+    }
+    
+    
 }
 
